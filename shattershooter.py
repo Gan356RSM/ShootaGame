1b615629486757eb84a1690cae3fe4bc369e4ce7
Gan356RSM <151418866+Gan356RSM@users.noreply.github.com>
2024-05-12 12:08:02 +0700

    Update shattershooter.py

--- shattershooter.py
+++ shattershooter.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 pygame.init()
 
@@ -51,19 +52,20 @@ class Lemon():
             self.rect.y = sh
 
 class Bullet():
-    def __init__(self, x, y, w, h, xs, ys, pic):
-        self.image = pygame.transform.scale(pygame.image.load(pic),(w, h))
+    def __init__(self, x, y):
+        self.image = pygame.transform.scale(pygame.image.load("bullet.png"),(75, 35))
         self.rect = self.image.get_rect()
-        self.rect.x = x
+        self.rect.x = x + 12   
         self.rect.y = y
-        self.xs  = xs
-        self.ys = ys
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
     def update(self):
-        pass
+        self.rect.y -= 10
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
 
 class Player(GameS):
     def update(self):
@@ -74,27 +76,72 @@ class Player(GameS):
         if keys[pygame.K_d]:
             self.rect.x += self.speed
 
-bullet = Bullet("Bullet.png", 0, 0, 0)
 player = Player("ROCKE.png", 350, 470, 4)
-L1 = Lemon(0, 0, 100, 70, 0, 2, "Lemon.png")
-L2 = Lemon(200, 0, 100, 70, 0, 2, "Lemon.png")
-L3 = Lemon(400, 0, 35, 23, 0, 4, "Lemon.png")
-L4 = Lemon(600, 0, 200, 140, 0, 1, "Lemon.png")
-lemons = [L1, L2, L3, L4]
-
+lemons = []
+bullets = []
 
 run = True
 finish = False
 
+f = pygame.font.Font("font.ttf", 48)
+lose_msg = f.render("You Lose!", True, (0, 0, 0))
+final_msg = None
+  
 while run:
+    if len(lemons) < 5:
+        lemons.append(Lemon(randint(0 ,750), 0, 200, 140, 0, 1, "lemon.png"))
+    
+
+
     for e in pygame.event.get():
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_SPACE:
+                bullets.append(Bullet(player.rect.x, player.rect.y))
         if e.type == pygame.QUIT:
             run = False
+
+    for l in lemons:
+        for b in bullets:
+            if l.rect.colliderect(b.rect):
+                if l.rect.w == 200:
+                    for i in range(3):
+                        lemons.append(Lemon(l.rect.x,
+                                               l.rect.y,
+                                               100,
+                                               70,
+                                               randint(-2, 2),
+                                               randint(-2, 2),
+                                               "lemon.png"))
+                if l.rect.w == 100:
+                    for i in range(7):
+                        lemons.append(Lemon(l.rect.x,
+                                               l.rect.y,
+                                               35,
+                                               25,
+                                               randint(-4, 4),
+                                               randint(-4, 4),
+                                               "lemon.png"))
+                lemons.remove(l)
+                bullets.remove(b)
+
 
     win.fill((46, 43, 43))
     for lime in lemons:
         lime.draw(win)
         lime.update()
+        if player.rect.colliderect(lime):
+            final_msg = lose_msg
+    
+    for b in bullets:
+        b.draw(win)
+        b.update()
+        if b.rect.y < 0:
+            bullets.remove(b)
+    
+
+
+    if final_msg != None:
+        win.blit(final_msg, (375, 275))
 
 
     player.update()
@@ -103,4 +150,6 @@ while run:
     
     pygame.display.update()
     clock.tick(30)
+
+
 
