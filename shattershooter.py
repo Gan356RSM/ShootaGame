9b28f8a868ce05d0fb6eab77f568f4e858c96412
Gan356RSM <151418866+Gan356RSM@users.noreply.github.com>
2024-06-02 12:05:02 +0700

    Update shattershooter.py

--- shattershooter.py
+++ shattershooter.py
@@ -1,155 +1,184 @@
 import pygame
-from random import randint
+import math
+import random
 
-pygame.init()
-
-# pygame.mixer.init()
-# pygame.music.load("fire.egg")
-# pygame.mixer.music.play()
-# watever = pygame.mixer.Sound("???")
-
-sh = 550
-sw = 750
-
-win = pygame.display.set_mode((sw, sh))
+win = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
-class GameS(pygame.sprite.Sprite):
-    def __init__(self, image, x, y, acc):
-        super().__init__()
-        self.image = pygame.transform.scale(pygame.image.load(image), (100, 65))
-        self.speed = acc
+class Enemy():
+    def __init__(self, x, y, player):
+        self.default_image = pygame.transform.scale(pygame.image.load("Lemon.png"), (50, 50))
+        self.default_image = pygame.transform.rotate(self.default_image, -90)
+        self.image = self.default_image
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-    
-    def reset(self):
-        win.blit(self.image, (self.rect.x, self.rect.y))
+        mx, my = player.rect.centerx, player.rect.centery
+        dx = mx - self.rect.centerx
+        dy = my - self.rect.centery
+        angle = math.degrees(math.atan2(-dy, dx))
+        self.hor = math.cos(math.radians(angle)) * 2
+        self.ver = -math.sin(math.radians(angle)) * 2
+        
 
-class Lemon():
-    def __init__(self, x, y, w, h, xs, ys, pic):
-        self.image = pygame.transform.scale(pygame.image.load(pic),(w, h))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.xs  = xs
-        self.ys = ys
+    def update(self):
+        self.rect.x += self.hor
+        self.rect.y += self.ver
+
+
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
-
-    def update(self):
-        self.rect.x += self.xs
-        self.rect.y += self.ys
-        if self.rect.x > sw + 100:
-            self.rect.x = 0
-        if self.rect.x < -100:
-            self.rect.x = sw
-
-        if self.rect.y > sh + 100:
-            self.rect.y = 0
-        if self.rect.y < -100:
-            self.rect.y = sh
 
 class Bullet():
-    def __init__(self, x, y):
-        self.image = pygame.transform.scale(pygame.image.load("bullet.png"),(75, 35))
+    def __init__(self, x, y, angle):
+        self.default_image = pygame.transform.scale(pygame.image.load("Bullet.png"),(75, 35))
+        self.default_image = pygame.transform.rotate(self.default_image, -90)
+        self.image = self.default_image
+        self.image = pygame.transform.rotate(self.image, angle)
         self.rect = self.image.get_rect()
-        self.rect.x = x + 12   
+        self.rect.centerx = x
+        self.rect.centery = y
+        self.xv = math.cos(math.radians(angle)) * 20
+        self.yv = -math.sin(math.radians(angle)) * 20
+        
+
+    def update(self):
+        self.rect.x += self.xv
+        self.rect.y += self.yv
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+class Gun():
+    def shoot(self, x, y, angle):
+        raise Exception("Gun must have shoot methods")
+    
+class Laser(Gun):
+    def __init__(self):
+        self.auto = False
+    def shoot(self, x, y, angle):
+        return [Bullet(x, y, angle)]
+
+class Bomb(Gun):
+    def __init__(self):
+        self.auto = False
+    def shoot(self, x, y, angle):
+        bullets = []
+        for i in range(5):
+            bullets.append(Bullet(x, y, angle + random.randint(-5, 5)))
+        return bullets
+    
+class Auto(Gun):
+    def __init__(self):
+        self.auto = True
+    def shoot(self, x, y, angle):
+        return [Bullet(x, y, angle)]
+
+
+class Ship():
+    def __init__(self, x, y):
+        self.default_image = pygame.transform.scale(pygame.image.load("ROCKE.png"), (100, 70))
+        self.default_image = pygame.transform.rotate(self.default_image, -90)
+        self.image = self.default_image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
         self.rect.y = y
+        self.bullets = []
+        self.gun = [Laser(), Bomb(), Auto()]
+        self.index = 0
+        
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+    def update(self, events, keys):
+        mx, my = pygame.mouse.get_pos()
+        dx = mx - self.rect.centerx
+        dy = my - self.rect.centery
+        angle = math.degrees(math.atan2(-dy, dx))
 
-    def update(self):
-        self.rect.y -= 10
+        old_x, old_y = self.rect.centerx, self.rect.centery
+        self.image = pygame.transform.rotate(self.default_image, angle)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = old_x
+        self.rect.centery = old_y
+        for e in events:
+                if e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_e:
+                        self.index += 1
+                        if self.index > 2:
+                            self.index = 0
+                        print(self.index)
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+        if not self.gun[self.index].auto:
+            for e in events:
+                if e.type == pygame.MOUSEBUTTONDOWN:
+                    self.bullets += self.gun[self.index].shoot(self.rect.centerx, self.rect.centery, angle)
+        else:
+            if pygame.mouse.get_pressed()[0]:
+                self.bullets += self.gun[self.index].shoot(self.rect.centerx, self.rect.centery, angle)
+        
+        for b in self.bullets:
+            b.update()
 
-class Player(GameS):
-    def update(self):
-        keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
-            self.rect.x -= self.speed
-
+            self.rect.x -= 2
         if keys[pygame.K_d]:
-            self.rect.x += self.speed
+            self.rect.x += 2
+        if keys[pygame.K_w]:
+            self.rect.y -= 2
+        if keys[pygame.K_s]:
+            self.rect.y += 2
 
-player = Player("ROCKE.png", 350, 470, 4)
-lemons = []
-bullets = []
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+        for b in self.bullets:
+            b.draw(surface)
+
+ship = Ship(200, 200)
+
+def Lemon_Spawner():
+    location = random.choice(["left", "right"])
+    if location == "left":
+        return Enemy(-50, random.randint(0, 600), ship)
+    if location == "right":
+        return Enemy(800, random.randint(0, 600), ship)
+
+enemies = []
+
+Amount_O_Lemon = 1
+frames = 0
 
 run = True
-finish = False
-
-f = pygame.font.Font("font.ttf", 48)
-lose_msg = f.render("You Lose!", True, (0, 0, 0))
-final_msg = None
-  
 while run:
-    if len(lemons) < 5:
-        lemons.append(Lemon(randint(0 ,750), 0, 200, 140, 0, 1, "lemon.png"))
-    
+    frames += 1
+    if frames >= 50:
+        for i in range(Amount_O_Lemon):
+            enemies.append(Lemon_Spawner())
+        frames = 0
+        Amount_O_Lemon += 1
 
+    events = pygame.event.get()
+    keys = pygame.key.get_pressed()
 
-    for e in pygame.event.get():
-        if e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_SPACE:
-                bullets.append(Bullet(player.rect.x, player.rect.y))
+    for e in events:
         if e.type == pygame.QUIT:
             run = False
 
-    for l in lemons:
-        for b in bullets:
-            if l.rect.colliderect(b.rect):
-                if l.rect.w == 200:
-                    for i in range(3):
-                        lemons.append(Lemon(l.rect.x,
-                                               l.rect.y,
-                                               100,
-                                               70,
-                                               randint(-2, 2),
-                                               randint(-2, 2),
-                                               "lemon.png"))
-                if l.rect.w == 100:
-                    for i in range(7):
-                        lemons.append(Lemon(l.rect.x,
-                                               l.rect.y,
-                                               35,
-                                               25,
-                                               randint(-4, 4),
-                                               randint(-4, 4),
-                                               "lemon.png"))
-                lemons.remove(l)
-                bullets.remove(b)
-
+    ship.update(events, keys)
+    for e in enemies:
+        e.update()
+        for b in ship.bullets:
+            if e.rect.colliderect(b.rect):
+                ship.bullets.remove(b)
+                enemies.remove(e)
+                break
+    
 
     win.fill((46, 43, 43))
-    for lime in lemons:
-        lime.draw(win)
-        lime.update()
-        if player.rect.colliderect(lime):
-            final_msg = lose_msg
-    
-    for b in bullets:
-        b.draw(win)
-        b.update()
-        if b.rect.y < 0:
-            bullets.remove(b)
+    ship.draw(win)
+    for e in enemies:
+        e.draw(win)
     
 
-
-    if final_msg != None:
-        win.blit(final_msg, (375, 275))
-
-
-    player.update()
-    player.reset()
-
-    
     pygame.display.update()
     clock.tick(30)
-
-
-
