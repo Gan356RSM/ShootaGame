1dc02884c37af8af749e1703a2a13b266fc30a65
Gan356RSM <151418866+Gan356RSM@users.noreply.github.com>
2024-06-23 12:03:18 +0700

    Update shattershooter.py

--- shattershooter.py
+++ shattershooter.py
@@ -6,17 +6,21 @@ win = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
 class Enemy():
-    def __init__(self, x, y, player):
-        self.default_image = pygame.transform.scale(pygame.image.load("Lemon.png"), (50, 50))
+    def __init__(self, x, y, player, w, h):
+        self.default_image = pygame.transform.scale(pygame.image.load("Lemon.png"), (w, h))
         self.default_image = pygame.transform.rotate(self.default_image, -90)
         self.image = self.default_image
         self.rect = self.image.get_rect()
+        print(self.rect.w, self.rect.h)
         self.rect.x = x
         self.rect.y = y
         mx, my = player.rect.centerx, player.rect.centery
         dx = mx - self.rect.centerx
         dy = my - self.rect.centery
-        angle = math.degrees(math.atan2(-dy, dx))
+        if self.rect.w == 140 and self.rect.h == 200:  
+            angle = math.degrees(math.atan2(-dy, dx))
+        if self.rect.w == 70 and self.rect.h == 100:
+            angle = random.randint(0, 360)
         self.hor = math.cos(math.radians(angle)) * 2
         self.ver = -math.sin(math.radians(angle)) * 2
         
@@ -24,6 +28,16 @@ class Enemy():
     def update(self):
         self.rect.x += self.hor
         self.rect.y += self.ver
+        if self.rect.x < -200:
+            self.rect.x = 950
+        elif self.rect.x > 950:
+            self.rect.x = -200
+        if self.rect.y < -100:
+            self.rect.y = 700
+        elif self.rect.y > 700:
+            self.rect.y = -100
+
+
 
 
 
@@ -137,11 +151,15 @@ class Ship():
 ship = Ship(200, 200)
 
 def Lemon_Spawner():
-    location = random.choice(["left", "right"])
+    location = random.choice(["left", "right","up","down"])
     if location == "left":
-        return Enemy(-50, random.randint(0, 600), ship)
+        return Enemy(-200, random.randint(0, 600), ship, 200, 140)
     if location == "right":
-        return Enemy(800, random.randint(0, 600), ship)
+        return Enemy(950, random.randint(0, 600), ship, 200, 140)
+    if location == "up":
+        return Enemy(random.randint(0, 800), -100, ship, 200, 140)
+    if location == "down":
+        return Enemy(random.randint(0, 800), 700, ship, 200, 140)
 
 enemies = []
 
@@ -149,13 +167,16 @@ Amount_O_Lemon = 1
 frames = 0
 
 run = True
+spawn_time = 300
 while run:
     frames += 1
-    if frames >= 50:
+    if frames >= spawn_time:
         for i in range(Amount_O_Lemon):
             enemies.append(Lemon_Spawner())
+        spawn_time -= 1
         frames = 0
         Amount_O_Lemon += 1
+        print(spawn_time)
 
     events = pygame.event.get()
     keys = pygame.key.get_pressed()
@@ -171,6 +192,12 @@ while run:
             if e.rect.colliderect(b.rect):
                 ship.bullets.remove(b)
                 enemies.remove(e)
+                if e.rect.w == 140 and e.rect.h == 200:  
+                    for ee in range(5):
+                        ee = Enemy(e.rect.x, e.rect.y, ship, 100, 70)
+                        enemies.append(ee)
+                        print('heloo')
+
                 break
     
 
